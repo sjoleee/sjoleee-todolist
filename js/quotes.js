175b3823b321c27286e2d3d45f59592ce4dcc54b
sjoleee <cent7425@gmail.com>
2021-08-06 15:03:28 +0900

    login complete

---
+++ js/quotes.js
@@ -0,0 +1,20 @@
+const quotes = [
+  { quote: "a", author: "aa" },
+  { quote: "b", author: "bb" },
+  { quote: "c", author: "cc" },
+  { quote: "d", author: "dd" },
+  { quote: "e", author: "ee" },
+  { quote: "f", author: "ff" },
+  { quote: "g", author: "gg" },
+  { quote: "h", author: "hh" },
+  { quote: "i", author: "ii" },
+  { quote: "j", author: "jj" },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const num = Math.floor(Math.random() * quotes.length);
+const todaysQuote = quotes[num];
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;

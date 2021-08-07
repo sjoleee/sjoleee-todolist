fe7ca023b862a5fae839655ec13c15629207b467
sjoleee <cent7425@gmail.com>
2021-08-08 01:26:11 +0900

    name change

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,8 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greetingDiv = document.querySelector("#greeting-div");
 const greeting = document.querySelector("#greeting");
+const greetingBtn = document.querySelector("#greeting-btn");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -15,13 +17,31 @@ function onLoginSubmit(event) {
 
 function paintGreetings(username) {
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  greetingBtn.classList.remove(HIDDEN_CLASSNAME);
   greeting.innerText = `Hello, ${username}`;
   document.querySelector("#todo-div").classList.remove("hidden");
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+const changeForm = document.querySelector("#change-form");
+const changeInput = document.querySelector("#change-form input");
 
-const greetings = document.querySelector("#greeting");
+function changeName(event) {
+  greeting.classList.add(HIDDEN_CLASSNAME);
+  greetingBtn.classList.add(HIDDEN_CLASSNAME);
+  changeForm.classList.remove(HIDDEN_CLASSNAME);
+}
+greetingBtn.addEventListener("click", changeName);
+
+function newName(event) {
+  event.preventDefault();
+  const newUsername = changeInput.value;
+  localStorage.setItem(USERNAME_KEY, newUsername);
+  paintGreetings(newUsername);
+  changeForm.classList.add(HIDDEN_CLASSNAME);
+}
+
+changeForm.addEventListener("submit", newName);
 
 if (savedUsername == null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
